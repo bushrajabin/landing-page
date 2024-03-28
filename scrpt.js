61b931834b42ff5775a1cc097e754d4a4cd6022a
Bushra jabin <bushrabintjabin@gmail.com>
2024-03-28 18:59:41 +0530

    Added blogsinglepage

--- scrpt.js
+++ scrpt.js
@@ -1,7 +1,6 @@
 const showLinks = () => {
   // document.getElementById("navbar-nav-links").style.display = "flex";
   alert("hi")
-  // document.getElementById("select-links-2").style.display="none"
 };
 
 
