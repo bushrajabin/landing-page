4fc212489ea340fd65319870133831753e2de7d5
Bushra jabin <bushrabintjabin@gmail.com>
2024-03-28 11:28:02 +0530

    signup-signin form created

--- scrpt.js
+++ scrpt.js
@@ -1,5 +1,6 @@
 const showLinks = () => {
-  document.getElementById("select-links-2").style.display = "flex";
+  // document.getElementById("navbar-nav-links").style.display = "flex";
+  alert("hi")
   // document.getElementById("select-links-2").style.display="none"
 };
 
